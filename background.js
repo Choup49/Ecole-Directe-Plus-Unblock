7abf655a61b7aaf7415ea26da671f95ab50133d3
Orion <154090769+Choup49@users.noreply.github.com>
2025-01-24 00:06:43 +0100

    test ?

--- background.js
+++ background.js
@@ -1,24 +1,37 @@
+// Modification des en-têtes de requête avant l'envoi
 chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-        details.requestHeaders.push({
-            name: "Access-Control-Allow-Origin",
-            value: "*"
-        });
-        return { requestHeaders: details.requestHeaders };
+    function (details) {
+      details.requestHeaders.push({
+        name: "Access-Control-Allow-Origin",
+        value: "*",
+      });
+      return { requestHeaders: details.requestHeaders };
     },
-    { urls: ["https://ecole-directe.plus/*", "https://www.ecoledirecte.com/*"] },
+    {
+      urls: [
+        "https://ecole-directe.plus/*",
+        "https://www.ecoledirecte.com/*",
+        "https://api.ecoledirecte.com/*", // URL de l'API spécifique
+      ],
+    },
     ["blocking", "requestHeaders"]
-);
-
-
-        headers.push(
-            { name: "Access-Control-Allow-Origin", value: "*" },
-            { name: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-            { name: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
-        );
-
-        return { responseHeaders: headers };
+  );
+  
+  // Modification des en-têtes de réponse
+  chrome.webRequest.onHeadersReceived.addListener(
+    function (details) {
+      let headers = details.responseHeaders || [];
+      headers.push(
+        { name: "Access-Control-Allow-Origin", value: "*" },
+        { name: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+        { name: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
+      );
+  
+      return { responseHeaders: headers };
     },
-    { urls: ["https://api.ecoledirecte.com/*"] }, // Remplacez par les URLs spécifiques si besoin
+    {
+      urls: ["https://api.ecoledirecte.com/*"], // Spécifique à l'API Ecole Directe
+    },
     ["blocking", "responseHeaders"]
-);
+  );
+  
